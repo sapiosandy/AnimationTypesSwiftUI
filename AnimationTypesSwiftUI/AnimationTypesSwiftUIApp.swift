f1b2588a617269b3694b7d9c3c7420d1989ebecb
Sandy Gomez <sgomez1029@gmail.com>
2024-05-27 15:18:05 -0700

    Initial Commit

---
+++ AnimationTypesSwiftUI/AnimationTypesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTypesSwiftUIApp.swift
+//  AnimationTypesSwiftUI
+//
+//  Created by Sandra Gomez on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTypesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
